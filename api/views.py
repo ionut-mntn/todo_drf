cf3b76975ff8695bf3a327be937d4327cac4839a
Ionut Muntean <ionutm@Ionuts-MacBook-Pro.local>
2022-08-29 22:12:07 +0300

    add some serializers

--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
 
+from .models import Task
 # Create your views here.
 
 @api_view(['GET', 'POST'])
@@ -18,3 +20,16 @@ def apiOverview(request):
     }
     # return Response("API BASE POINT", safe=False, context=api_urls)
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
