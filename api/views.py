2314944ed92720f0a5df05523e2d69dfd2a40aef
Ionut Muntean <ionutm@Ionuts-MacBook-Pro.local>
2022-09-10 16:49:01 +0300

    finish api

--- api/views.py
+++ api/views.py
@@ -33,3 +33,39 @@ def taskDetail(request, pk):
     serializer = TaskSerializer(task, many=False)
     return Response(serializer.data)
 
+@api_view(['POST'])
+def taskCreate(request):
+    print(f"request.data in taskCreate: {request.data}")
+    serializer = TaskSerializer(data=request.data) # normally, we do request.POST, but since this is an API view, we have access to request.data!
+    
+    if serializer.is_valid():
+        serializer.save()
+        # return Response(serializer.data)
+        
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    print(f"request.data in taskUpdate: {request.data}")
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data) # ca la forms! We set the instance and then update it!
+    
+    if serializer.is_valid():
+        serializer.save()
+        # return Response(serializer.data)
+        
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    print(f"request.data in taskDelete: {request.data}")
+    
+    # ori asa:
+    # SomeModel.objects.filter(id=id).delete()
+    
+    # ori dupa ce ne luam instanta
+    task = Task.objects.get(id=pk)
+    task.delete()
+    
+    return Response('Item successfully deleted!')
